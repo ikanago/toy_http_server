a3d41107e5cb0ad83247449731143e58ad593fb1
ikanago <28985004+ikanago@users.noreply.github.com>
2020-06-28 15:36:54 +0900

    ✨ Just print incoming request

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
-use toy_http_server;
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
 
-fn main() {
-    let hello = toy_http_server::hello();
-    println!("{}", hello);
+fn handle_client(stream: &mut TcpStream) {
+    let mut buf = [0; 512];
+    stream.read(&mut buf).expect("Error reading stream");
+    let result = String::from_utf8(buf.to_vec()).unwrap();
+    println!("{}", result);
+    println!("All read");
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:1111")?;
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut s) => {
+                handle_client(&mut s);
+            }
+            Err(e) => eprintln!("{:?}", e),
+        }
+    }
+    Ok(())
 }
